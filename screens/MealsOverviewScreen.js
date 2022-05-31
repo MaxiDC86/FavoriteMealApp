3934f9ce2782b8425f7c04d4a235561a7f7c2f56
maxidc86 <max-rub@hotmail.com>
2022-05-31 20:39:54 -0300

    Context for the Favorites added

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,5 +1,4 @@
-import { FlatList, View, Text, StyleSheet } from "react-native";
-import MealItem from "../components/MealItem";
+import MealsList from "../components/MealsList/MealsList";
 import { MEALS, CATEGORIES } from "../data/dummy-data";
 import { useLayoutEffect } from "react";
 
@@ -19,36 +18,6 @@ function MealsOverviewScreen({ route, navigation }) {
     });
   }, [catId, navigation]);
 
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      duration: item.duration,
-      complexity: item.complexity,
-      affordability: item.affordability,
-    };
-
-    return <MealItem {...mealItemProps} />;
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList items={displayedMeals} />;
 }
-
 export default MealsOverviewScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
